091b582f45059f076a1650c00c8903645d01d549
alexstyl <1665273+alexstyl@users.noreply.github.com>
2024-09-01 17:50:38 +0700

    Replace Indication with IndicationNodeFactory

--- core/src/commonMain/kotlin/FocusRingIndication.kt
+++ core/src/commonMain/kotlin/FocusRingIndication.kt
@@ -1,14 +1,18 @@
 package com.composables.core
 
 import androidx.compose.foundation.Indication
-import androidx.compose.foundation.IndicationInstance
+import androidx.compose.foundation.IndicationNodeFactory
+import androidx.compose.foundation.interaction.FocusInteraction
 import androidx.compose.foundation.interaction.InteractionSource
-import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
@@ -18,9 +22,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.ContentDrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.node.DelegatableNode
+import androidx.compose.ui.node.DrawModifierNode
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 
 @Composable
@@ -30,52 +36,75 @@ public fun rememberFocusRingIndication(
     paddingValues: PaddingValues = PaddingValues(),
     cornerRadius: Dp
 ): Indication {
-    return remember { FocusRingIndication(ringColor, ringWidth, paddingValues, cornerRadius) }
+    return remember { FocusRingIndicationNodeFactory(ringColor, ringWidth, paddingValues, cornerRadius) }
 }
 
-internal class FocusRingIndication internal constructor(
+internal class FocusRingIndicationNodeFactory internal constructor(
     private val ringColor: Color,
     private val strokeWidth: Dp,
     private val paddingValues: PaddingValues,
     private val cornerRadius: Dp,
-) : Indication {
+) : IndicationNodeFactory {
 
-    @Composable
-    override fun rememberUpdatedInstance(interactionSource: InteractionSource): IndicationInstance {
-        val isFocused = interactionSource.collectIsFocusedAsState()
-        val layoutDirection = LocalLayoutDirection.current
-        return remember(interactionSource, layoutDirection) {
-            object : IndicationInstance {
-                override fun ContentDrawScope.drawIndication() {
-                    drawContent()
-                    if (isFocused.value) {
-                        val cornerRadiusObj = CornerRadius(cornerRadius.toPx())
-                        val ringWidthFloat = strokeWidth.toPx()
+    override fun create(interactionSource: InteractionSource): DelegatableNode {
+        return FocusRingIndicationNode(interactionSource, ringColor, strokeWidth, paddingValues, cornerRadius)
+    }
 
-                        val paddingFloatTop = paddingValues.calculateTopPadding().toPx()
-                        val paddingFloatBottom = paddingValues.calculateBottomPadding().toPx()
-                        val paddingFloatStart = paddingValues.calculateStartPadding(layoutDirection).toPx()
-                        val paddingFloatEnd = paddingValues.calculateEndPadding(layoutDirection).toPx()
+    internal class FocusRingIndicationNode internal constructor(
+        private val interactionSource: InteractionSource,
+        private val ringColor: Color,
+        private val strokeWidth: Dp,
+        private val paddingValues: PaddingValues,
+        private val cornerRadius: Dp,
+    ) : Modifier.Node(), DrawModifierNode {
 
-                        val ringSize = Size(
-                            width = size.width + paddingFloatStart + paddingFloatEnd,
-                            height = size.height + paddingFloatTop + paddingFloatBottom
+        var isFocused by mutableStateOf(false)
+
+
+        override fun ContentDrawScope.draw() {
+            drawContent()
+            if (isFocused) {
+                val cornerRadiusObj = CornerRadius(cornerRadius.toPx())
+                val ringWidthFloat = strokeWidth.toPx()
+
+                val paddingFloatTop = paddingValues.calculateTopPadding().toPx()
+                val paddingFloatBottom = paddingValues.calculateBottomPadding().toPx()
+                val paddingFloatStart = paddingValues.calculateStartPadding(layoutDirection).toPx()
+                val paddingFloatEnd = paddingValues.calculateEndPadding(layoutDirection).toPx()
+
+                val ringSize = Size(
+                    width = size.width + paddingFloatStart + paddingFloatEnd,
+                    height = size.height + paddingFloatTop + paddingFloatBottom
+                )
+
+                val topLeft = Offset(-paddingFloatStart, -paddingFloatTop)
+
+                val ringPath = Path().apply {
+                    addRoundRect(
+                        RoundRect(
+                            rect = Rect(offset = topLeft, size = ringSize),
+                            cornerRadius = cornerRadiusObj
                         )
+                    )
+                }
+                drawPath(ringPath, color = ringColor, style = Stroke(width = ringWidthFloat))
+            }
+        }
 
-                        val topLeft = Offset(-paddingFloatStart, -paddingFloatTop)
-
-                        val ringPath = Path().apply {
-                            addRoundRect(
-                                RoundRect(
-                                    rect = Rect(offset = topLeft, size = ringSize),
-                                    cornerRadius = cornerRadiusObj
-                                )
-                            )
-                        }
-                        drawPath(ringPath, color = ringColor, style = Stroke(width = ringWidthFloat))
+        override fun onAttach() {
+            coroutineScope.launch {
+                interactionSource.interactions.collectLatest { interaction ->
+                    when (interaction) {
+                        is FocusInteraction.Focus -> isFocused = true
+                        is FocusInteraction.Unfocus -> isFocused = true
                     }
                 }
             }
+            super.onAttach()
         }
     }
+
+    override fun hashCode(): Int = -1
+
+    override fun equals(other: Any?) = other === this
 }
